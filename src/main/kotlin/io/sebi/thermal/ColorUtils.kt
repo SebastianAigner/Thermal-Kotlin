0a5e0740f9bc992ea4f75cc862be53f576a8eb0b
kapil-777 <111303107+kapil-777@users.noreply.github.com>
2024-01-12 11:23:13 +0530

    Update ColorUtils.kt
    enhance readability

--- src/main/kotlin/io/sebi/thermal/ColorUtils.kt
+++ src/main/kotlin/io/sebi/thermal/ColorUtils.kt
@@ -4,13 +4,13 @@ import java.awt.Color
 
 fun Color.grayscaled(): Color {
     val value = (this.red + this.green + this.blue) / 3
-    return Color(value, value, value)
+    return Color(value, value, value) //return the color of same value in Color class constructor 
 }
 
 fun Color.blackOrWhite(): Color {
     return if ((this.red + this.green + this.blue) / 3 > 128) {
-        Color.white
+        Color.white  //if white
     } else {
-        Color.black
+        Color.black //else black
     }
 }
